6e543a3538aa657741b92877d0d451c81c436c27
Paulo Leme <paulo.oph@hotmail.com>
2024-10-03 16:24:07 -0300

    Primeiro commit

---
+++ AlunosAPI/Program.cs
@@ -0,0 +1,44 @@
+using AlunosAPI.Context;
+using AlunosAPI.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Criando conexão com o banco de dados
+builder.Services.AddDbContext<AppDbContext>(o =>
+{
+    o.UseSqlServer(builder.Configuration.GetConnectionString("DataBase"));
+});
+
+builder.Services.AddScoped<IAlunoService, AlunosService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(options =>
+{
+    options.WithOrigins("http://localhost:3000")
+           .AllowAnyMethod()
+           .AllowAnyHeader();
+});
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
